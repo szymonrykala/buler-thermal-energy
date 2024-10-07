665fbfae6d6a9e3694aff37a163ace622b19b475
Szymon Rykała <szymonrykala@gmail.com>
2024-10-07 23:37:08 +0200

    delete cached views

--- storage/framework/views/4eafa53f1a06b96982c1c5903f6324ae.blade.php
+++
@@ -1,6 +0,0 @@
-<?php extract(collect($attributes->getAttributes())->mapWithKeys(function ($value, $key) { return [Illuminate\Support\Str::camel(str_replace([':', '.'], ' ', $key)) => $value]; })->all(), EXTR_SKIP); ?>
-
-<x-icons.fw-panel  >
-
-{{ $slot ?? "" }}
-</x-icons.fw-panel>
--- storage/framework/views/6a8e7b545802a8fbcdc845ffdc66f5ba.php
+++
@@ -1,4 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
-    <path d="M450-180v-485.08L222.15-437.23 180-480l300-300 300 300-42.15 42.77L510-665.08V-180h-60Z" />
-</svg>
-<?php /**PATH /Users/szymeg/Projects/buler_energy/resources/views/components/icons/arrow-up.blade.php ENDPATH**/ ?>
